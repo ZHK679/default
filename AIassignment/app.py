261a2eeb64c28b42c46c1f44e7abe30d53e0cdba
ZHK679 <99705463+ZHK679@users.noreply.github.com>
2022-02-27 20:45:54 +0800

    Add files via upload

---
+++ AIassignment/app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+from flask import request, render_template
+from keras.models import load_model
+
+@app.route("/",methods=["GET","POST"])
+
+def index():
+    if request.method=="POST":
+        income=request.form.get("income")
+        age=request.form.get("age")
+        loan=request.form.get("loan")
+        print(income,age,loan)
+        model = load_model("Default")
+        
+        #account for normalisation using normalisation formula (x – x minimum) / (x maximum – x minimum)
+        
+        pred=model.predict([[float((float(income)-20014.48947)/(69995.68558-20014.48947)),float((float(age)-18.05518851)/(63.97179584-18.05518851)),float((float(loan)-1.377629593)/(13766.05124-1.377629593))]])
+        print(pred)
+        s= "The predicted probability of default is:" +str(pred)
+        
+        
+        return(render_template("index.html",results=s))
+    
+    else:
+        return(render_template("index.html",results="Predict Default"))
+    
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
